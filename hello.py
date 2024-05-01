5053fcab563521ab8cefc90a2c1e7b613d2aee8f
Japesa112 <130290441+Japesa112@users.noreply.github.com>
2024-05-01 23:45:02 +0300

    Update hello.py

--- hello.py
+++ hello.py
@@ -22,5 +22,5 @@ def transcribe_audio():
         return jsonify({'error': 'No audio URL provided'})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8080)
+    app.run(debug=False, host='0.0.0.0', port=8080)
 
